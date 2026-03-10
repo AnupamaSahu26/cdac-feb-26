1a80defdf2340b4e90831cdc9b81b1c1abd182e3
AnupamaSahu26 <anupama.sahu.cmfeb26@gmail.com>
2026-03-10 13:52:02 +0530

    Create c++ass3q5.cpp

---
+++ c++ass3q5.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class Number
+{
+private:
+    int value;
+
+public:
+    Number(int v){ value=v; }
+
+    Number& operator++()
+    {
+        value++;
+        return *this;
+    }
+
+    Number operator++(int)
+    {
+        Number temp=*this;
+        value++;
+        return temp;
+    }
+
+    Number& operator=(Number n)
+    {
+        value=n.value;
+        return *this;
+    }
+
+    friend bool operator>(Number a,Number b);
+
+    friend class Inspector;
+};
+
+bool operator>(Number a,Number b)
+{
+    return a.value>b.value;
+}
+
+class Inspector
+{
+public:
+    void show(Number n)
+    {
+        cout<<"Value: "<<n.value<<endl;
+    }
+};
+
+int main()
+{
+    Number n1(10),n2(20);
+
+    ++n1;
+    n2++;
+
+    Inspector i;
+    i.show(n1);
+
+    if(n2>n1)
+        cout<<"n2 greater";
+
+    return 0;
+}
