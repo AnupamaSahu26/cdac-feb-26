2399ff82b96bf5fe22869c99810fdfabbf303d39
AnupamaSahu26 <anupama.sahu.cmfeb26@gmail.com>
2026-03-10 13:47:02 +0530

    Create c++ass3q2.cpp

---
+++ c++ass3q2.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class String
+{
+public:
+    char *str;
+
+    String()
+    {
+        str=new char[100];
+    }
+
+    String(const char *s)
+    {
+        str=new char[strlen(s)+1];
+        strcpy(str,s);
+    }
+
+    String(const String &s)
+    {
+        str=new char[strlen(s.str)+1];
+        strcpy(str,s.str);
+    }
+
+    ~String()
+    {
+        delete[] str;
+    }
+
+    String operator+(String s)
+    {
+        String temp;
+
+        temp.str = new char[strlen(str) + strlen(s.str) + 1];
+
+        strcpy(temp.str,str);
+        strcat(temp.str,s.str);
+
+        return temp;
+    }
+
+    friend ostream& operator<<(ostream &out,const String &s)
+    {
+        out<<s.str;
+        return out;
+    }
+
+    friend istream& operator>>(istream &in,String &s)
+    {
+        in>>s.str;
+        return in;
+    }
+};
+
+int main()
+{
+    String s1,s2;
+
+    cin>>s1>>s2;
+
+    String s3=s1+s2;
+
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+    cout<<s3<<endl;
+
+    return 0;
+}
