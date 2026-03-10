07e910fdfd76bded2c00e73cd60313bdb5ad786e
AnupamaSahu26 <anupama.sahu.cmfeb26@gmail.com>
2026-03-10 13:49:21 +0530

    Create c++ass3q4.cpp

---
+++ c++ass3q4.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class DivideByZero{};
+
+int divide(int a,int b) throw(DivideByZero)
+{
+    if(b==0)
+        throw DivideByZero();
+
+    return a/b;
+}
+
+int main()
+{
+    try
+    {
+        try
+        {
+            cout<<divide(10,0);
+        }
+        catch(DivideByZero)
+        {
+            cout<<"Inside inner catch"<<endl;
+            throw;
+        }
+    }
+    catch(DivideByZero)
+    {
+        cout<<"Handled in outer catch"<<endl;
+    }
+
+    return 0;
+}
